9bf990c326a4692fb8c5cf376bc7c4bb36e35ca8
pooja suresh <Poojasurendran40@gmail.com>
2023-10-10 23:30:11 +0530

    fix

--- app.js
+++ app.js
@@ -28,32 +28,46 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-// Define an async function to insert default items
-async function insertDefaultItems() {
-  try {
-    const result = await Item.insertMany(defaultItems);
-    console.log(`Inserted ${result.length} default items successfully.`);
-  } catch (err) {
-    console.error(`Error inserting default items: ${err}`);
-  }
-}
 
-// Call the async function to insert default items
-insertDefaultItems();
-
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
 app.get("/", async function (req, res) {
   try {
-    const foundItems = await Item.find({});
-    res.render("list", { KindOfDay: "Today", newListItems: foundItems });
+    const foundItems = await Item.find({}); // Use async/await to handle the promise
+
+    if (foundItems.length === 0) {
+      await Item.insertMany(defaultItems); // Use async/await here as well
+      console.log("Successfully saved in the database");
+      res.redirect("/");
+    } else {
+      res.render("list", { KindOfDay: "Today", newListItems: foundItems });
+    }
+    app.post("/delete", async function(req, res){
+      const checkedItemId = req.body.checkbox;
+    
+      try {
+        await Item.findByIdAndRemove(checkedItemId);
+        res.redirect("/");
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+    
   } catch (err) {
     console.error(err);
     res.status(500).send("Internal Server Error");
   }
 });
+
+ 
+
+
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
 
 app.post("/", async function (req, res) {
   const itemName = req.body.newItem;
